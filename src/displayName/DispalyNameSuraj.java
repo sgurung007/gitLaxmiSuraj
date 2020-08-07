3da3d6eeda4a8cc845466b7b539c532c1ba2c651
Suraj Gurung <laxhu2008@gmail.com>
2020-08-07 10:36:24 -0500

    commit2

--- src/displayName/DispalyNameSuraj.java
+++ src/displayName/DispalyNameSuraj.java
@@ -3,5 +3,6 @@ package displayName;
 public class DispalyNameSuraj {
 	public static void main(String[] args) {
 		System.out.println("this is first line");
+		System.out.println("this is second line");
 	}
 }
