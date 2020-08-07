f8fd28828a2340885417a15b0b8e7a5e5f7a4858
Suraj Gurung <laxhu2008@gmail.com>
2020-08-07 10:43:41 -0500

    suraj1 commit 1

--- src/displayName/DispalyNameSuraj.java
+++ src/displayName/DispalyNameSuraj.java
@@ -4,5 +4,6 @@ public class DispalyNameSuraj {
 	public static void main(String[] args) {
 		System.out.println("this is first line");
 		System.out.println("this is second line");
+		System.out.println("suraj1 this is suraj1");
 	}
 }
